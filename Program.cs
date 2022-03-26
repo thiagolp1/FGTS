2f88427b203d0c3c33bc11d962dcbf7b4929a910
Thiago <Thiagolopespereiralp1@gmail.com>
2022-03-25 21:55:35 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double salario, FGTS;
+
+Console.WriteLine("Salário (R$)..:");
+salario = Convert.ToDouble(Console.ReadLine());
+
+FGTS = (salario / 100) * 8;
+
+Console.WriteLine($"\nFGTS: {FGTS:C2}");
